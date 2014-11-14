4e729849dad7462fa719335483a8b9fd3c96a780
soufianebaroudi <soufiane.barodi@gmail.com>
2014-11-14 14:51:57 +0100

    deuxième histoire

--- test/test.js
+++ test/test.js
@@ -5,6 +5,12 @@ var PalettoTestCase = TestCase("PalettoTestCase");
 PalettoTestCase.prototype.testStory1 = function () {
 
     var game = new paletto();
-    assertTrue (game.isColor()== 0);
+    assertTrue(game.isColor() == 0);
 
+};
+
+PalettoTestCase.prototype.testStory2 = function () {
+
+    var game = new paletto();
+    assertTrue(game.isYellow() == "yellow");
 };
